960dbf389abcc571cc931f4b3c463d62dcdb1f5a
huyande <1473280483@qq.com>
2024-12-06 13:40:55 +0800

    fix:增加事件处理

--- src/main/java/com/tencent/wxcloudrun/controller/WxEventController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/WxEventController.java
@@ -1,6 +1,9 @@
 package com.tencent.wxcloudrun.controller;
 
 import cn.hutool.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/event")
 public class WxEventController {
 
+    final Logger logger;
+
+    public WxEventController() {
+        this.logger = LoggerFactory.getLogger(WxEventController.class);;
+    }
+
     @PostMapping("/handel")
     public String handel(@RequestBody JSONObject json) {
-        System.out.printf(json.toString());
-        return "ok";
+        logger.info("事件响应：",json.toString());
+        return "";
     }
 }
